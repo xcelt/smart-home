ead0646473a85c912dcecc138a63b7fea7c54fe1
Lee Saxton <107041075+lauxton@users.noreply.github.com>
2023-10-26 00:14:17 +0700

    Add files via upload

--- assignment2/backend/temperature/models.py
+++ assignment2/backend/temperature/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from collections import Counter # make sure in reqs.txt
 
-# Create your models here.
+
+def tempDefault():
+    tempDefault = 22
+
+def increaseTemp(tempDefault):
+    for int in tempDefault:
+        increase=int+1
+        increase.save()
+    return increase
+    
+
+def decreaseTemp(tempDefault):
+    for int in tempDefault:
+        decrease=int-1
+        decrease.save()
+    return decrease
