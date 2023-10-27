f95c55aace920e03201f6ce8995c83a9913d4d60
Xue Ling Teh <107624269+xcelt@users.noreply.github.com>
2023-10-27 21:02:34 +1100

    Add tests for alarm and sensor classes

---
+++ model/test_alarm.py
@@ -0,0 +1,42 @@
+import pytest
+from microservice import Microservice
+from model.alarm import Alarm
+
+
+@pytest.fixture
+def initialise_alarms():
+    sensorAlarm = Alarm(name="Senza", type=Microservice.SENSOR)
+    thermostatAlarm = Alarm("Thano", Microservice.THERMOSTAT)
+    cameraAlarm = Alarm("Cameron", Microservice.CAMERA)
+    lightAlarm = Alarm("Lightsaber", Microservice.LIGHT)
+    smartlockAlarm = Alarm("Sherlock", Microservice.SMART_LOCK)
+
+
+# def test_microservice():
+#     assert len(Microservice) == 5
+
+
+# def test_alarm():
+#     initialise_alarms()
+#     alarmList = Alarm.get_alarms()
+#     for alarm in alarmList:
+#         print(alarm.enabled)
+#     Alarm.display_alarms()
+
+
+def test_alarm_get_alarms(initialise_alarms):
+    assert len(Alarm.get_alarms()) == 5
+
+
+# @pytest.mark.parametrize("", initialise_alarms())
+# def test_alarm_enable_all():
+#     Alarm.enable_all()
+#     for alarm in Alarm.__alarmList:
+#         assert alarm.enabled
+
+#     # Alarm.enable_all()
+#     # Alarm.disable_all()
+
+
+# test_alarm()
+# test_alarm_enable_all()
